f8efff0e88d9b49dc965b9e99a5d767e1c5f1c7b
Hallison <hallison.o.b@gmail.com>
2022-12-14 16:54:17 -0300

    Alteração de vídeo e validação de "Send Image"

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,7 +3,7 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyles = createGlobalStyle`
     @font-face {
         font-family: 'Anteb-Regular';
-        src: local('Anteb-Regular'), url('./fonts/Anteb-Regular.ttf') format('ttf');
+        src: local('Anteb-Regular'), url('./fonts/Anteb-Regular.ttf') format('truetype');
         font-style: normal;
         font-weight: normal;
     }
