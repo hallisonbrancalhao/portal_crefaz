c3d3a1f7325aa2c76acf219d3c64b4ee0ff04637
Hallison <hallison.o.b@gmail.com>
2022-12-14 16:30:03 -0300

    Alteração de vídeo e validação de "Send Image"

--- src/pages/ScreenVideo/index.tsx
+++ src/pages/ScreenVideo/index.tsx
@@ -14,7 +14,7 @@ const Apresentacao = () => {
         <div className="col-md-8 mb-5 offset-md-2 d-flex justify-content-center">
           <div className='inputs transition mt-5 mx-5'>
             <video className='video ratio ratio-16-9' width="750px" controls controlsList='nodownload' >
-              <source src={require("../../assets/crefaz.mp4")} type="video/mp4" />
+              <source src={require("../../assets/Manifesto_Crefaz_Loc_Kassia.mp4")} type="video/mp4" />
             </video>
             <button className='px-5 py-2 btn btn-secondary my-5' onClick={() => { navigate('/perfil') }}>
               <span>Continuar</span>
